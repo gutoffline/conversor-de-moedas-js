0d695595e18e9d67026fad15611f163099b7cbe0
Guto Xavier <gutoffline@gmail.com>
2021-03-31 20:10:18 -0300

    Refatoração

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,16 +1,4 @@
-let valorEmDolarTexto = prompt("Qual o valor em dólar que você quer converter?");
-console.log( typeof valorEmDolarTexto);
-// string = texto
-
-let valorEmDolarNumero = parseFloat(valorEmDolarTexto);
-console.log(typeof valorEmDolarNumero);
-// float = número decimal -> 12.99
-// int = número inteiro -> 12
-
-alert(valorEmDolarNumero);
-
-let valorEmReal = valorEmDolarNumero * 5.77;
-
+let valorEmDolar = parseFloat(prompt("Qual o valor em dólar que você quer converter?"));
+let valorEmReal = valorEmDolar * 5.77;
 let valorEmRealFixado = valorEmReal.toFixed(2);
-
 alert(valorEmRealFixado);
